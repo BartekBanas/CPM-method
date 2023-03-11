bce60c311dc4a7fd5d065a4b98bc537ce21227fa
Bartek Banaś <bartek.banas675@gmail.com>
2023-03-12 00:08:01 +0100

    Creating first controllers

---
+++ Backend/Project Planner/ProjectPlanner.Application/Controllers/CpmController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectPlanner.Application.Services;
+
+namespace Project_Planner.Controllers;
+
+[ApiController]
+[Microsoft.AspNetCore.Components.Route("api/CPM")]
+public class CpmController
+{
+    private readonly CpmTaskService _cpmTaskService;
+}
