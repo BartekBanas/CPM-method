cf8f88b3342c0a5c47cef5f9f595389d3cb4d8dc
Bartek Banaś <bartek.banas675@gmail.com>
2023-03-15 22:19:40 +0100

    ValidateAmountOfActivities

--- Backend/Project Planner/ProjectPlanner.Business.Validation/CpmTaskValidator.cs
+++ Backend/Project Planner/ProjectPlanner.Business.Validation/CpmTaskValidator.cs
@@ -1,5 +1,30 @@
-﻿namespace ProjectPlanner.Business.Validation;
+﻿using ProjectPlanner.Infrastructure.TaskObjects;
+
+namespace ProjectPlanner.Business.Validation;
 
 public class CpmTaskValidator
 {
+    public CpmTask Task { get; }
+
+    public CpmTaskValidator(CpmTask task)
+    {
+        Task = task;
+    }
+
+    public bool Validate()
+    {
+        
+    }
+
+    public bool ValidateAmountOfActivities()
+    {
+        if (Task.Activities.Count < 2)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 }
