ffcaf0af7b2b140b4ff31ccffac87f86b4b246b0
Bartek Banaś <bartek.banas675@gmail.com>
2023-03-15 22:22:51 +0100

    Changing the idea of CpmTaskValidator

--- Backend/Project Planner/ProjectPlanner.Business.Validation/CpmTaskValidator.cs
+++ Backend/Project Planner/ProjectPlanner.Business.Validation/CpmTaskValidator.cs
@@ -5,21 +5,24 @@ namespace ProjectPlanner.Business.Validation;
 public class CpmTaskValidator
 {
     public CpmTask Task { get; }
+    public bool Validity { get; set; }
 
     public CpmTaskValidator(CpmTask task)
     {
         Task = task;
+        Validity = true;
     }
 
     public bool Validate()
     {
-        
+        return Validity;
     }
 
     public bool ValidateAmountOfActivities()
     {
         if (Task.Activities.Count < 2)
         {
+            Validity = false;
             return false;
         }
         else
