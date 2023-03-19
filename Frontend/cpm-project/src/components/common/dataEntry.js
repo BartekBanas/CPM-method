6ee08d12cf1c31c781aeec25331e92a180d820e1
burdelio <burdelio19@gmail.com>
2023-03-19 19:44:23 +0100

    Adjusted and finished datatab panel

--- Frontend/cpm-project/src/components/common/dataEntry.js
+++ Frontend/cpm-project/src/components/common/dataEntry.js
@@ -25,14 +25,30 @@ function DataEntryForNewTask() {
                             <Input />
                         </Form.Item>
                         <Form.Item name="nastepstwozdarzen" label="Nastepstwo Zdarzeń" rules={[{ required: true }]}>
-                            <Input />
+                            <Form.Item
+                                name="zd1"
+                                rules={[{ required: true }]}
+                                style={{ display: 'inline-block', width: 'calc(50% - 3px)' }}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <div style={{ display: 'inline-block' }}>
+                                -
+                            </div>
+                            <Form.Item
+                                name="zd2"
+                                rules={[{ required: true }]}
+                                style={{ display: 'inline-block', width: 'calc(50% - 3px)' }}
+                            >
+                                <Input />
+                            </Form.Item>
                         </Form.Item>
                         <Form.Item>
-                            <Button type="primary" htmlType="submit">
+                            <Button type="primary" htmlType="submit" style={{ display: 'inline-block', margin: '0 5%' }}>
                                 Zatwierdź
                             </Button>
-                            <Button htmlType="button" onClick={onReset}>
-                                Resetuj
+                            <Button htmlType="button" onClick={onReset} style={{ display: 'inline-block', margin: '0 5%' }}>
+                                Reset
                             </Button>
                         </Form.Item>
                     </Card>
