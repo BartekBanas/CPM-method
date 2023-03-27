5fce40b7379b816eea14512cdd3a8b12f1bbb20d
Bartek Banaś <bartek.banas675@gmail.com>
2023-03-27 23:23:15 +0200

    Disabling ValidateLooping, cause it clearly doesn't work

--- Backend/Project Planner/ProjectPlanner.Business.Validation/CpmTaskValidator.cs
+++ Backend/Project Planner/ProjectPlanner.Business.Validation/CpmTaskValidator.cs
@@ -19,7 +19,7 @@ public class CpmTaskValidator
         //ValidateAmountOfActivities();
         ValidateSequences();
         ValidateStartAndEnd();
-        ValidateLooping();
+        //ValidateLooping();
         
         return Validity;
     }
