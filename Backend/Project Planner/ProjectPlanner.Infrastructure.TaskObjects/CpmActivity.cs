8540d718c3a028e19dcc8d2c3e001ae7d7c0e745
Bartek Banaś <bartek.banas675@gmail.com>
2023-03-27 23:25:13 +0200

    Updating CpmActivity

--- Backend/Project Planner/ProjectPlanner.Infrastructure.TaskObjects/CpmActivity.cs
+++ Backend/Project Planner/ProjectPlanner.Infrastructure.TaskObjects/CpmActivity.cs
@@ -2,11 +2,9 @@
 
 public class CpmActivity
 {
-    public int TaskID { get; set; }
     public string TaskName { get; set; }
     public int Duration { get; set; }
     public int[] Sequence { get; set; }
-    public bool IsCritical { get; set; }
 
     public CpmActivity(string taskName, int duration, int[] sequence)
     {
