b9f9650234e9df02e0dbcf492955c56a58bbf38e
Bartek Banaś <bartek.banas675@gmail.com>
2023-03-28 09:24:11 +0200

    Adding scope for CpmService

--- Backend/Project Planner/ProjectPlanner.Application/Program.cs
+++ Backend/Project Planner/ProjectPlanner.Application/Program.cs
@@ -1,3 +1,5 @@
+using ProjectPlanner.Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,8 @@ services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
+
+services.AddScoped<ICpmService, CpmService>();
 
 var app = builder.Build();
 
