2a581eaeb6abd7e9fa63122035100c2e1c7f83ac
Bartek Banaś <bartek.banas675@gmail.com>
2023-03-31 10:45:17 +0200

    Extending CpmSolution

--- Backend/Project Planner/ProjectPlanner.Infrastructure.SolutionObjects/CpmSolution.cs
+++ Backend/Project Planner/ProjectPlanner.Infrastructure.SolutionObjects/CpmSolution.cs
@@ -5,4 +5,6 @@ namespace ProjectPlanner.Infrastructure.SolutionObjects;
 public class CpmSolution
 {
     public List<CpmActivity> Activities { get; set; } = null!;
+
+    public int CriticalTime { get; set; }
 }
