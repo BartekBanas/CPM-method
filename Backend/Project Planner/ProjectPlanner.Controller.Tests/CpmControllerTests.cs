7f03d1c932e3143c5ae9baa375b3110a2f3da471
Bartek Banaś <bartek.banas675@gmail.com>
2023-04-19 19:48:28 +0200

    Cleanup

--- Backend/Project Planner/ProjectPlanner.Controller.Tests/CpmControllerTests.cs
+++ Backend/Project Planner/ProjectPlanner.Controller.Tests/CpmControllerTests.cs
@@ -11,14 +11,12 @@ namespace ProjectPlanner.Controller.Tests;
 
 public class CpmControllerTests
 {
-    private readonly ITestOutputHelper _testOutputHelper;
     private readonly Mock<ICpmService> _cpmServiceMock;
     private readonly Mock<IValidator<CpmTask>> _validatorMock;
     private readonly CpmController _controller;
 
     public CpmControllerTests(ITestOutputHelper testOutputHelper)
     {
-        _testOutputHelper = testOutputHelper;
         _cpmServiceMock = new Mock<ICpmService>();
         _validatorMock = new Mock<IValidator<CpmTask>>();
         _controller = new CpmController(_cpmServiceMock.Object, _validatorMock.Object);
@@ -107,7 +105,11 @@ public class CpmControllerTests
         _validatorMock.Setup(x => x.ValidateAsync(It.IsAny<CpmTask>(), CancellationToken.None))
             .ReturnsAsync(new FluentValidation.Results.ValidationResult
             {
-                Errors = { new FluentValidation.Results.ValidationFailure("", "A cyclic dependency between activities has been detected") }
+                Errors =
+                {
+                    new FluentValidation.Results.ValidationFailure("",
+                        "A cyclic dependency between activities has been detected")
+                }
             });
 
         // Act
@@ -119,7 +121,7 @@ public class CpmControllerTests
         Assert.NotEmpty(errors);
         Assert.Equal("A cyclic dependency between activities has been detected", errors[0].ErrorMessage);
     }
-    
+
     [Fact]
     public async Task PostCpmRequest_ReturnsBadRequest_ForInvalidRequestWithInvalidSequence()
     {
@@ -151,9 +153,11 @@ public class CpmControllerTests
             {
                 Errors =
                 {
-                    new FluentValidation.Results.ValidationFailure("Activities", "Activity 2 cannot come in between one and the same event"),
+                    new FluentValidation.Results.ValidationFailure("Activities",
+                        "Activity 2 cannot come in between one and the same event"),
                     new FluentValidation.Results.ValidationFailure("Activities", "More than one starting event found"),
-                    new FluentValidation.Results.ValidationFailure("", "A cyclic dependency between activities has been detected")
+                    new FluentValidation.Results.ValidationFailure("",
+                        "A cyclic dependency between activities has been detected")
                 }
             });
 
