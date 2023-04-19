b62d3950873103e2f89ba2ce35ee1718557c8d9a
Bartek Banaś <bartek.banas675@gmail.com>
2023-04-19 18:12:43 +0200

    Test for Bad CPM request

--- Backend/Project Planner/ProjectPlanner.Controller.Tests/CpmControllerTests.cs
+++ Backend/Project Planner/ProjectPlanner.Controller.Tests/CpmControllerTests.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using Newtonsoft.Json;
 using Project_Planner.Controllers;
 using ProjectPlanner.Application.Services;
 using ProjectPlanner.Business.CriticalPathMethod;
@@ -50,4 +51,66 @@ public class CpmControllerTests
         // Assert
         Assert.IsType<OkObjectResult>(result);
     }
+    
+    [Fact]
+    public async Task PostCpmRequest_ReturnsBadRequest_ForInvalidRequestWithCycle()
+    {
+        // Arrange
+        var json = @"{
+        ""activities"": [
+            {
+                ""taskName"": ""Task 1"",
+                ""duration"": 3,
+                ""sequence"": [0, 1]
+            },
+            {
+                ""taskName"": ""Task 2"",
+                ""duration"": 5,
+                ""sequence"": [1, 2]
+            },
+            {
+                ""taskName"": ""Task 3"",
+                ""duration"": 2,
+                ""sequence"": [2, 3]
+            },
+            {
+                ""taskName"": ""Task 3"",
+                ""duration"": 2,
+                ""sequence"": [3, 4]
+            },
+            {
+                ""taskName"": ""Task 3"",
+                ""duration"": 2,
+                ""sequence"": [4, 2]
+            },
+            {
+                ""taskName"": ""Task 3"",
+                ""duration"": 2,
+                ""sequence"": [4, 5]
+            }
+        ]
+    }";
+
+        var task = JsonConvert.DeserializeObject<CpmTask>(json);
+
+        // _validatorMock.Setup(v => v.ValidateAsync(task)).ReturnsAsync(new FluentValidation.Results.ValidationResult
+        // {
+        //     Errors = { new FluentValidation.Results.ValidationFailure("", "A cyclic dependency between activities has been detected") }
+        // });
+
+        // Act
+        if (task != null)
+        {
+            var result = await _controller.PostCpmRequest(task);
+
+            _testOutputHelper.WriteLine(result.ToString());
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var errors = Assert.IsType<List<FluentValidation.Results.ValidationFailure>>(badRequestResult.Value);
+            Assert.NotEmpty(errors);
+            Assert.Equal("A cyclic dependency between activities has been detected", errors[0].ErrorMessage);
+        }
+    }
+
 }
