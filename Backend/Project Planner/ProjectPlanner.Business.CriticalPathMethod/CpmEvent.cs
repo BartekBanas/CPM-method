5c4eee7443a699a51ed76ebbc7b3f74e77525f33
Bartek Banaś <bartek.banas675@gmail.com>
2023-04-22 20:24:12 +0200

    Humble beginnings for CPM algorithm

--- Backend/Project Planner/ProjectPlanner.Business.CriticalPathMethod/CpmEvent.cs
+++ Backend/Project Planner/ProjectPlanner.Business.CriticalPathMethod/CpmEvent.cs
@@ -2,5 +2,33 @@
 
 public class CpmEvent
 {
-    
+    public int Id { get; set; }
+    public int EarliestStart { get; set; }
+    public int LatestStart { get; set; }
+    public int EarliestFinish { get; set; }
+    public int LatestFinish { get; set; }
+    public int Slack { get; set; }
+
+    public CpmEvent(int id, int earliestStart, int latestStart, int earliestFinish, int latestFinish)
+    {
+        Id = id;
+        EarliestStart = earliestStart;
+        LatestStart = latestStart;
+        EarliestFinish = earliestFinish;
+        LatestFinish = latestFinish;
+    }
+
+    public CpmEvent(int id, int earliestStart, int latestStart, int earliestFinish, int latestFinish, int slack)
+    {
+        Id = id;
+        EarliestStart = earliestStart;
+        LatestStart = latestStart;
+        EarliestFinish = earliestFinish;
+        LatestFinish = latestFinish;
+        Slack = slack;
+    }
+
+    public CpmEvent()
+    {
+    }
 }
--- Backend/Project Planner/ProjectPlanner.Business.CriticalPathMethod/CpmProject.cs
+++ Backend/Project Planner/ProjectPlanner.Business.CriticalPathMethod/CpmProject.cs
@@ -6,6 +6,74 @@ public class CpmProject
 {
     public CpmSolution CreateSolution(CpmTask task)
     {
-        return new CpmSolution();
+        SetupActivities(task);
+        
+        var activities = task.Activities;
+
+        // Calculate earliest and latest start times for each activity
+        var earliestStartTimes = new Dictionary<int, int>();
+        var latestStartTimes = new Dictionary<int, int>();
+        var eventDictionary = new Dictionary<int, CpmEvent>();
+
+        int earliestStart = 0;
+        foreach (var activity in activities)
+        {
+            int id = activity.Id;
+            earliestStartTimes[id] = earliestStart;
+
+            int latestStart = int.MaxValue;
+            foreach (var otherActivity in activities)
+            {
+                if (activity.Sequence[1] == otherActivity.Sequence[0])
+                {
+                    latestStart = Math.Min(latestStart, earliestStartTimes[otherActivity.Id] - otherActivity.Duration);
+                }
+            }
+
+            latestStartTimes[id] = latestStart;
+            earliestStart = earliestStartTimes[id] + activity.Duration;
+
+            eventDictionary[id] = new CpmEvent
+            {
+                Id = id,
+                EarliestStart = earliestStartTimes[id],
+                LatestStart = latestStartTimes[id],
+                EarliestFinish = earliestStartTimes[id] + activity.Duration,
+                LatestFinish = latestStartTimes[id] + activity.Duration,
+                Slack = latestStartTimes[id] - earliestStartTimes[id]
+            };
+        }
+
+        // Find critical path and mark critical activities
+        var criticalPath = new List<int>();
+        foreach (var activity in activities)
+        {
+            if (eventDictionary[activity.Id].Slack == 0)
+            {
+                activity.Critical = true;
+                criticalPath.Add(activity.Id);
+            }
+            else
+            {
+                activity.Critical = false;
+            }
+        }
+
+        // Build solution object
+        var solution = new CpmSolution
+        {
+            Activities = activities,
+            Events = eventDictionary.Values.ToList()
+        };
+
+        return solution;
+    }
+
+    private void SetupActivities(CpmTask task)
+    {
+        for (int i = 0; i < task.Activities.Count; i++)
+        {
+            task.Activities[i].Id = i;
+        }
     }
 }
