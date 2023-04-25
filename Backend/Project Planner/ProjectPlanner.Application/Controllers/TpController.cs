b4ce180c7c934ced8fce6936ac92ffc80b83f2e3
Bartek Banaś <bartek.banas675@gmail.com>
2023-04-25 12:41:45 +0200

    Adding attribute to TpController method

--- Backend/Project Planner/ProjectPlanner.Application/Controllers/TpController.cs
+++ Backend/Project Planner/ProjectPlanner.Application/Controllers/TpController.cs
@@ -15,6 +15,7 @@ public class TpController : Controller
         _tpService = tpService;
     }
     
+    [HttpPost]
     public async Task<IActionResult> PostCpmRequest([FromBody] TpTask task)
     {
         // var validationResult = await _validator.ValidateAsync(task);
