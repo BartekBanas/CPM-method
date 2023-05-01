5b4e6cf13e2ac3590ac1a9e042943d96afc0720e
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-01 12:18:01 +0200

    Suppliers validation

--- Backend/Project Planner/ProjectPlanner.Business.Validation/TpFluentValidator.cs
+++ Backend/Project Planner/ProjectPlanner.Business.Validation/TpFluentValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using ProjectPlanner.Business.TransportationProblem;
 using ProjectPlanner.Business.TransportationProblem.Dtos;
 
 namespace ProjectPlanner.Business.Validation;
@@ -14,6 +15,23 @@ public class TpFluentValidator : AbstractValidator<TpTask>
         RuleFor(tpTask => tpTask.Recipients)
             .NotNull().WithMessage("Recipients cannot be null")
             .Must(recipients => recipients.Length > 0).WithMessage("There must be at least one recipient");
+
+        RuleFor(tpTask => tpTask.Suppliers).Custom(ValidateSuppliers);
     }
 
+    private void ValidateSuppliers(Supplier[] suppliers ,ValidationContext<TpTask> context)
+    {
+        for (int i = 0; i < suppliers.Length; i++)
+        {
+            if (suppliers[i].Supply <= 0)
+            {
+                context.AddFailure("Supplier " + (i + 1) + " needs to have supply");
+            }
+
+            if (suppliers[i].Cost <= 0)
+            {
+                context.AddFailure("Supplier " + (i + 1) + " needs to have positive selling cost");
+            }
+        }
+    }
 }
