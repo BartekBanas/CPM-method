0033073050a6077a46e4b5933b1a0ccdc0094284
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-01 12:20:30 +0200

    Recipients validation

--- Backend/Project Planner/ProjectPlanner.Business.Validation/TpFluentValidator.cs
+++ Backend/Project Planner/ProjectPlanner.Business.Validation/TpFluentValidator.cs
@@ -17,6 +17,7 @@ public class TpFluentValidator : AbstractValidator<TpTask>
             .Must(recipients => recipients.Length > 0).WithMessage("There must be at least one recipient");
 
         RuleFor(tpTask => tpTask.Suppliers).Custom(ValidateSuppliers);
+        RuleFor(task => task.Recipients).Custom(ValidateRecipients);
     }
 
     private void ValidateSuppliers(Supplier[] suppliers ,ValidationContext<TpTask> context)
@@ -31,6 +32,22 @@ public class TpFluentValidator : AbstractValidator<TpTask>
             if (suppliers[i].Cost <= 0)
             {
                 context.AddFailure("Supplier " + (i + 1) + " needs to have positive selling cost");
+            }
+        }
+    }
+    
+    private void ValidateRecipients(Recipient[] recipients ,ValidationContext<TpTask> context)
+    {
+        for (int i = 0; i < recipients.Length; i++)
+        {
+            if (recipients[i].Demand <= 0)
+            {
+                context.AddFailure("Recipient " + (i + 1) + " needs to have demand");
+            }
+
+            if (recipients[i].Cost <= 0)
+            {
+                context.AddFailure("Recipient " + (i + 1) + " needs to have positive buying cost");
             }
         }
     }
