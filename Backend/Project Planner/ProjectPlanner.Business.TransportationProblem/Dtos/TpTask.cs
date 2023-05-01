c648c377371295e871d728dfdd1b5aec01858da2
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-01 10:58:56 +0200

    Constructor for TpTask

--- Backend/Project Planner/ProjectPlanner.Business.TransportationProblem/Dtos/TpTask.cs
+++ Backend/Project Planner/ProjectPlanner.Business.TransportationProblem/Dtos/TpTask.cs
@@ -2,4 +2,15 @@
 
 public class TpTask
 {
+    public Supplier[] Suppliers { get; set; }
+    public Recipient[] Recipients { get; set; }
+
+    public float[,] TransportCost { get; set; }
+
+    public TpTask(Supplier[] suppliers, Recipient[] recipients, float[,] transportCost)
+    {
+        Suppliers = suppliers;
+        Recipients = recipients;
+        TransportCost = transportCost;
+    }
 }
