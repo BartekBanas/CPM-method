1d270a7744a0257180b90805bf3638fd0aa534af
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-01 11:24:35 +0200

    Establishing Tp validator

--- Backend/Project Planner/ProjectPlanner.Business.Validation/TpFluentValidator.cs
+++ Backend/Project Planner/ProjectPlanner.Business.Validation/TpFluentValidator.cs
@@ -1,6 +1,19 @@
-﻿namespace ProjectPlanner.Business.Validation;
+﻿using FluentValidation;
+using ProjectPlanner.Business.TransportationProblem.Dtos;
 
-public class TpFluentValidator
+namespace ProjectPlanner.Business.Validation;
+
+public class TpFluentValidator : AbstractValidator<TpTask>
 {
-    
+    public TpFluentValidator()
+    {
+        RuleFor(tpTask => tpTask.Suppliers)
+            .NotNull().WithMessage("Suppliers cannot be null")
+            .Must(suppliers => suppliers.Length > 0).WithMessage("There must be at least one supplier");
+
+        RuleFor(tpTask => tpTask.Recipients)
+            .NotNull().WithMessage("Recipients cannot be null")
+            .Must(recipients => recipients.Length > 0).WithMessage("There must be at least one recipient");
+    }
+
 }
