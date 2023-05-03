952f90693a19fa36d01813ea3ab9de679f5567e9
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-03 02:08:17 +0200

    Setup for TpControllerTests

--- Backend/Project Planner/Tests/ProjectPlanner.Controller.Tests/TpControllerTests.cs
+++ Backend/Project Planner/Tests/ProjectPlanner.Controller.Tests/TpControllerTests.cs
@@ -1,6 +1,15 @@
-﻿namespace ProjectPlanner.Controller.Tests;
+﻿using ProjectPlanner.Business.TransportationProblem.Dtos;
+
+namespace ProjectPlanner.Controller.Tests;
 
 public class TpControllerTests
 {
-    
+    private readonly TpController _controller;
+
+    public TpControllerTests()
+    {
+        ITpService cpmService = new TpService();
+        IValidator<TpTask> validator = new TpFluentValidator();
+        _controller = new TpController(cpmService, validator);
+    }
 }
