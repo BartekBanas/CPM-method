9562b64f29468d5f159cdd449d88f8d4f5ca2d2c
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-03 02:14:42 +0200

    First TP test

--- Backend/Project Planner/Tests/ProjectPlanner.Controller.Tests/TpControllerTests.cs
+++ Backend/Project Planner/Tests/ProjectPlanner.Controller.Tests/TpControllerTests.cs
@@ -8,8 +8,58 @@ public class TpControllerTests
 
     public TpControllerTests()
     {
-        ITpService cpmService = new TpService();
+        ITpService tpService = new TpService();
         IValidator<TpTask> validator = new TpFluentValidator();
-        _controller = new TpController(cpmService, validator);
+        _controller = new TpController(tpService, validator);
+    }
+
+    [Fact]
+    public async Task PostTpRequest_ReturnsOkResult_ForValidRequest()
+    {
+        // Arrange
+        const string jsonTpTask = @" 
+        {
+            {
+                ""Suppliers"": [
+            {
+                ""Supply"": 10,
+                ""Cost"": 5.0
+            },
+            {
+                ""Supply"": 20,
+                ""Cost"": 10.0
+            }
+            ],
+            ""Recipients"": [
+            {
+                ""Demand"": 5,
+                ""Cost"": 8.0
+            },
+            {
+                ""Demand"": 25,
+                ""Cost"": 12.0
+            },
+            {
+                ""Demand"": 15,
+                ""Cost"": 9.0
+            }
+            ],
+            ""TransportCost"": [
+            [5.0, 7.0, 6.0],
+            [4.0, 2.0, 8.0]
+            ]
+        }}";
+
+        var task = JsonConvert.DeserializeObject<TpTask>(jsonTpTask);
+
+        // Act
+        if (task != null)
+        {
+            var result = await _controller.PostTpRequest(task);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+        }
     }
 }
