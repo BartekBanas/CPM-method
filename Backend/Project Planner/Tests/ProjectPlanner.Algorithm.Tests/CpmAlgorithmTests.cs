53f40fc94f42854961558fd837831ce9a85221ec
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-10 14:10:04 +0200

    Test for Late Start CPM

--- Backend/Project Planner/Tests/ProjectPlanner.Algorithm.Tests/CpmAlgorithmTests.cs
+++ Backend/Project Planner/Tests/ProjectPlanner.Algorithm.Tests/CpmAlgorithmTests.cs
@@ -293,4 +293,57 @@ public class CpmAlgorithmTests
         Assert.Equal(solution.Activities[2].EarlyFinish, 10);
         Assert.Equal(solution.Activities[3].EarlyFinish, 4);
     }
+    
+    [Fact]
+    public void CreateSolution_ShouldIdentifyLateStart()
+    {
+        // Arrange
+        var cpmTask = new CpmTask
+        {
+            Activities = new List<CpmActivity>
+            {
+                new CpmActivity
+                {
+                    TaskName = "Task 1",
+                    Duration = 3,
+                    Sequence = new int[] { 0, 1 }
+                },
+                new CpmActivity
+                {
+                    TaskName = "Task 2",
+                    Duration = 5,
+                    Sequence = new int[] { 1, 2 }
+                },
+                new CpmActivity
+                {
+                    TaskName = "Task 3",
+                    Duration = 2,
+                    Sequence = new int[] { 2, 3 }
+                },
+                new CpmActivity
+                {
+                    TaskName = "Task 3",
+                    Duration = 1,
+                    Sequence = new int[] { 1, 3 }
+                }
+            }
+        };
+        
+        var project = new CpmProject(cpmTask);
+
+        // Act
+        var solution = project.CreateSolution();
+
+        // Assert
+        Assert.NotNull(solution);
+        Assert.NotNull(solution.Activities);
+        Assert.NotNull(solution.Events);
+        Assert.Equal(solution.Activities.Count, 4);
+        Assert.Equal(solution.Events.Count, 4);
+        
+        Assert.Equal(solution.Activities[0].LateStart, 0);
+        Assert.Equal(solution.Activities[1].LateStart, 3);
+        Assert.Equal(solution.Activities[2].LateStart, 8);
+        Assert.Equal(solution.Activities[3].LateStart, 9);
+    }
 }
