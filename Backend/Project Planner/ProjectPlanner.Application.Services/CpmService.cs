6f10fc85e43fcf9f299208fe2c8ab9bc5ed05faf
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-17 03:13:31 +0200

    Moving CPM validation from controller to service

--- Backend/Project Planner/ProjectPlanner.Application.Services/CpmService.cs
+++ Backend/Project Planner/ProjectPlanner.Application.Services/CpmService.cs
@@ -1,14 +1,32 @@
-﻿using ProjectPlanner.Business.CriticalPathMethod;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using ProjectPlanner.Business.CriticalPathMethod;
 
 namespace ProjectPlanner.Application.Services;
 
 public interface ICpmService
 {
+    public Task<ValidationResult> Validate(CpmTask task);
     Task<CpmSolution> Solve(CpmTask task);
 }
 
 public class CpmService : ICpmService
 {
+    private readonly IValidator<CpmTask> _validator;
+
+    public CpmService(IValidator<CpmTask> validator)
+    {
+        _validator = validator;
+    }
+
+    public async Task<ValidationResult> Validate(CpmTask task)
+    {
+        var validationResult = await _validator.ValidateAsync(task);
+
+        return validationResult;
+    }
+
+
     public Task<CpmSolution> Solve(CpmTask task)
     {
         CpmProject cpmProject = new CpmProject(task);
