ae33e6fe5104099bd8df0e89886bb21293d1398c
burdelio <burdelio19@gmail.com>
2023-05-18 17:10:54 +0200

    Final mediator table and few fixes in other components

---
+++ Frontend/cpm-project/src/components/common/DynamicTable/DynamicTable.js
@@ -0,0 +1,187 @@
+import { useState } from 'react';
+import { Button, Empty, Table, Space } from 'antd';
+import { DeleteTwoTone } from '@ant-design/icons';
+import EditableCell from './EditableCell';
+import EditableRow from './EditableRow';
+
+import './DynamicTable.css';
+
+const newColumnProps = {
+  title: '',
+  dataIndex: "new",
+  width: 120,
+  editable: true
+};
+
+const DynamicTableContent = ({ dataSource, columns, handleCellSave, handleAddRow, handleAddCol }) => {
+  const components = {
+    body: {
+      row: EditableRow,
+      cell: EditableCell
+    }
+  };
+
+  // for setting onCell - passing props per cell of this column
+  const mappedCols = columns.map((col) => {
+    if (!col.editable) {
+      return {
+        ...col,
+        onCell: (record) => ({
+          record,
+          rowScope: col.rowScope
+        })
+      };
+    }
+
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave: handleCellSave
+      })
+    };
+  });
+
+  return (
+    <div>
+      <Space>
+        <Button
+          onClick={handleAddRow}
+          type="primary"
+          style={{
+            marginBottom: 16
+          }}
+        >
+          Add a row
+        </Button>
+        <Button
+          onClick={handleAddCol}
+          type="primary"
+          style={{
+            marginBottom: 16
+          }}
+        >
+          Add a column
+        </Button>
+      </Space>
+      <Table
+        locale={{ emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="Wypełnij formularz" /> }}
+        components={components}
+        rowClassName={() => "editable-row"}
+        bordered
+        sticky
+        dataSource={dataSource}
+        columns={mappedCols}
+        style={{ maxWidth: '80%', margin: 'auto'}}
+        pagination={false}
+        scroll={{x: true}}
+      />
+    </div>
+  );
+};
+
+const DynamicTable = () => {
+  const [dataSource, setDataSource] = useState([]);
+  const [columns, setColumns] = useState([
+    {
+      title: "",
+      dataIndex: "key",
+      rowScope: "row",
+      align: "center",
+      width: 80,
+      fixed: 'left',
+      render: (text, record) => {
+        return (
+          <Space>
+            Dostawca {text}
+            <DeleteTwoTone onClick={() => handleDeleteRow(record.key)} />
+          </Space>
+        )
+      }
+    },
+    {
+      title: "",
+      dataIndex: "padding"
+    }
+  ]);
+
+  const handleAddRow = () => {
+    setDataSource((dataSource) => {
+      const last = dataSource.slice(-1);
+      const newData = {
+        key: ((last[0]?.key || 0) + 1),
+      };
+      return [...dataSource, newData]
+    });
+  };
+
+  const handleAddCol = () => {
+    setColumns((columns) => {
+      const newColumns = [...columns];
+      const keepLast = newColumns.pop();
+      const last = newColumns.slice(-1);
+      const id = ((last[0]?.key || 0) + 1);
+      const dataIndex = `dynamic${id}`;
+
+      return [
+        ...newColumns,
+        {
+          ...newColumnProps,
+          title: () => {
+            return (
+              <Space>
+                Odbiorca {id}
+                <DeleteTwoTone onClick={() => handleDeleteCol(dataIndex)} />
+              </Space>
+            );
+          },
+          dataIndex,
+          key: id
+        },
+        keepLast
+      ];
+    });
+  };
+
+  const handleCellSave = (row) => {
+    setDataSource((dataSource) => {
+      const newData = [...dataSource];
+      const index = newData.findIndex((item) => row.key === item.key);
+      const item = newData[index];
+
+      newData.splice(index, 1, {
+        ...item,
+        ...row
+      });
+
+      return newData;
+    });
+  };
+
+  const handleDeleteRow = (key) => {
+    setDataSource((dataSource) => {
+      return [...dataSource].filter((item) => item.key !== key);
+    });
+  };
+
+  const handleDeleteCol = (dataIndex) => {
+    setColumns((columns) => {
+      return [...columns].filter((item) => item.dataIndex !== dataIndex);
+    });
+    setDataSource((dataSource) => {
+      return [...dataSource].map((row) => { delete row[dataIndex]; return row;})
+    });
+  };
+
+  return <DynamicTableContent
+    dataSource={dataSource}
+    columns={columns}
+    handleCellSave={handleCellSave}
+    handleAddRow={handleAddRow}
+    handleAddCol={handleAddCol}
+  />;
+};
+export { DynamicTable as default };
