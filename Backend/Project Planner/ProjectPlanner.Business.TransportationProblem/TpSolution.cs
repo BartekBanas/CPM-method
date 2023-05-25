8fd5cd24e41aed462236f80f607200f2484846df
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-25 13:12:16 +0200

    TpSolution designed

--- Backend/Project Planner/ProjectPlanner.Business.TransportationProblem/TpSolution.cs
+++ Backend/Project Planner/ProjectPlanner.Business.TransportationProblem/TpSolution.cs
@@ -2,5 +2,18 @@
 
 public class TpSolution
 {
-    
+    public float TotalCost { get; set; }
+    public float TotalRevenue { get; set; }
+    public float TotalProfit { get; set; }
+
+    public float[][] OptimalTransportationTable { get; set; }
+
+    public TpSolution(float totalCost, float totalRevenue, float totalProfit, float[][] optimalTransportationTable)
+    {
+        TotalCost = totalCost;
+        TotalRevenue = totalRevenue;
+        TotalProfit = totalProfit;
+        OptimalTransportationTable = optimalTransportationTable ??
+                                     throw new ArgumentNullException(nameof(optimalTransportationTable));
+    }
 }
