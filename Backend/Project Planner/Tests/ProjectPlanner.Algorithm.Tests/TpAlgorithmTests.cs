e3b33f741bc5ff7db66ab20cce6313ee14c51d7f
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-26 18:33:51 +0200

    Test for Transportation Table in Tp algorithm

---
+++ Backend/Project Planner/Tests/ProjectPlanner.Algorithm.Tests/TpAlgorithmTests.cs
@@ -0,0 +1,43 @@
+﻿using ProjectPlanner.Business.TransportationProblem;
+
+namespace ProjectPlanner.Algorithm.Tests;
+
+public class TpAlgorithmTests
+{
+    [Fact]
+    public void CreateSolution_ShouldIdentifyTransportationTable()
+    {
+        // Arrange
+        List<Supplier> suppliers = new List<Supplier>
+        {
+            new Supplier(20, 10),
+            new Supplier(30, 12)
+        };
+
+        List<Recipient> recipients = new List<Recipient>
+        {
+            new Recipient(10, 30),
+            new Recipient(28, 25),
+            new Recipient(27, 30)
+        };
+
+        float[][] transportCost = new float[][]
+        {
+            new float[] { 8, 14, 17 },
+            new float[] { 12, 9, 19 }
+        };
+
+        TpTask tpTask = new TpTask(suppliers, recipients, transportCost);
+
+        var project = new TpProject(tpTask);
+        
+        // Act
+        var solution = project.CreateSolution();
+        
+        // Assert
+        Assert.NotNull(solution);
+        Assert.NotNull(solution.TransportationTable);
+        Assert.Equal(2, solution.TransportationTable.Length);
+        Assert.Equal(3, solution.TransportationTable[0].Length);
+    }
+}
