794ae747834834881e7aaf4fa91b66d6d949dcb1
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-26 18:04:07 +0200

    Name change

--- Backend/Project Planner/ProjectPlanner.Business.TransportationProblem/TpSolution.cs
+++ Backend/Project Planner/ProjectPlanner.Business.TransportationProblem/TpSolution.cs
@@ -6,18 +6,14 @@ public class TpSolution
     public float TotalRevenue { get; set; }
     public float TotalProfit { get; set; }
 
-    public float[][] OptimalTransportationTable { get; set; }
-
+    public float[][] TransportationTable { get; set; }
+    
     public TpSolution(float totalCost, float totalRevenue, float totalProfit, float[][] optimalTransportationTable)
     {
         TotalCost = totalCost;
         TotalRevenue = totalRevenue;
         TotalProfit = totalProfit;
-        OptimalTransportationTable = optimalTransportationTable ??
+        TransportationTable = optimalTransportationTable ??
                                      throw new ArgumentNullException(nameof(optimalTransportationTable));
-    }
-
-    public TpSolution()
-    {
     }
 }
