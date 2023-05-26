d193d97bb3eca7fa314b97d346f46e2bea9db00b
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-26 18:53:51 +0200

    Add test for Total Profit

--- Backend/Project Planner/Tests/ProjectPlanner.Algorithm.Tests/TpAlgorithmTests.cs
+++ Backend/Project Planner/Tests/ProjectPlanner.Algorithm.Tests/TpAlgorithmTests.cs
@@ -121,4 +121,39 @@ public class TpAlgorithmTests
         Assert.NotNull(solution.TotalRevenue);
         Assert.Equal(1300, solution.TotalRevenue);
     }
+    
+    [Fact]
+    public void CreateSolution_ShouldIdentifyTotalProfit()
+    {
+        // Arrange
+        List<Supplier> suppliers = new List<Supplier>
+        {
+            new Supplier(20, 10),
+            new Supplier(30, 12)
+        };
+
+        List<Recipient> recipients = new List<Recipient>
+        {
+            new Recipient(10, 30),
+            new Recipient(28, 25),
+            new Recipient(27, 30)
+        };
+
+        float[][] transportCost = new float[][]
+        {
+            new float[] { 8, 14, 17 },
+            new float[] { 12, 9, 19 }
+        };
+
+        TpTask tpTask = new TpTask(suppliers, recipients, transportCost);
+        TpProject project = new TpProject(tpTask);
+        
+        // Act
+        var solution = project.CreateSolution();
+        
+        // Assert
+        Assert.NotNull(solution);
+        Assert.NotNull(solution.TotalProfit);
+        Assert.Equal(262, solution.TotalProfit);
+    }
 }
