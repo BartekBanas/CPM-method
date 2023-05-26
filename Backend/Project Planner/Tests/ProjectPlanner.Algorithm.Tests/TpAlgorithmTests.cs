7b4fae56a0e2c7065feb1356da4ac6dc75c5bbec
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-26 18:50:40 +0200

    Add test for Transportation costs

--- Backend/Project Planner/Tests/ProjectPlanner.Algorithm.Tests/TpAlgorithmTests.cs
+++ Backend/Project Planner/Tests/ProjectPlanner.Algorithm.Tests/TpAlgorithmTests.cs
@@ -51,4 +51,39 @@ public class TpAlgorithmTests
             Assert.True(expectedTable[i].SequenceEqual(solution.TransportationTable[i]));
         }
     }
+    
+    [Fact]
+    public void CreateSolution_ShouldIdentifyTotalCost()
+    {
+        // Arrange
+        List<Supplier> suppliers = new List<Supplier>
+        {
+            new Supplier(20, 10),
+            new Supplier(30, 12)
+        };
+
+        List<Recipient> recipients = new List<Recipient>
+        {
+            new Recipient(10, 30),
+            new Recipient(28, 25),
+            new Recipient(27, 30)
+        };
+
+        float[][] transportCost = new float[][]
+        {
+            new float[] { 8, 14, 17 },
+            new float[] { 12, 9, 19 }
+        };
+
+        TpTask tpTask = new TpTask(suppliers, recipients, transportCost);
+        TpProject project = new TpProject(tpTask);
+        
+        // Act
+        var solution = project.CreateSolution();
+        
+        // Assert
+        Assert.NotNull(solution);
+        Assert.NotNull(solution.TotalCost);
+        Assert.Equal(1038, solution.TotalCost);
+    }
 }
